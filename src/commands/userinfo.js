f71cae8a03df78d123f0a0f1dbd8ca9b78637266
Luke-6723 <lukemorris2015@hotmail.com>
2019-10-14 23:23:41 +0200

    rewrote userinfo command in accordance with new command handler

--- src/commands/userinfo.js
+++ src/commands/userinfo.js
@@ -1,26 +1,31 @@
-const { Command } = require('./')
+const { Command } = require('.')
 
-module.exports = new Command('userinfo', ['ui'], 'Get user information', (client, msg, args) => {
-  let id
-  if (args.length === 0) id = msg.author.id
-  else id = args[0].replace(/[<@!>]+/g, '')
-  console.log(id)
-  client.users.fetch(id).then(user => {
-    const embed = new client.discord.MessageEmbed()
-      .setColor(client.embedColor)
-      .setThumbnail(user.avatarURL())
-      .addField('User Information:', `
+module.exports = class UserInfo extends Command {
+  constructor (client) {
+    super(client, ['userinfo', 'ui'], 'Get information about a user', 0)
+  }
+
+  run (msg, args) {
+    let id
+    if (args.length === 0) id = msg.author.id
+    else id = args[0].replace(/[<@!>]+/g, '')
+    this.client.users.fetch(id).then(user => {
+      const embed = new this.client.discord.MessageEmbed()
+        .setColor(this.client.embedColor)
+        .setThumbnail(user.avatarURL())
+        .addField('User Information:', `
 **Username**: \`${user.username}\`
 **Descriminator**: \`${user.discriminator}\`
 
 **Account Created**: \`${user.createdAt.toUTCString()}\`
 **Account Created (Time)**: 
-${client.convertTime((Date.now() - user.createdAt))}
+${this.client.convertTime((Date.now() - user.createdAt))}
 `)
-    msg.channel.send({ embed })
-  }).catch(() => {
-    msg.channel.send(new client.discord.MessageEmbed()
-      .addField('Error', 'User Not Found.')
-      .setColor(client.embedColor))
-  })
-}, 0)
+      msg.channel.send({ embed })
+    }).catch(() => {
+      msg.channel.send(new this.client.discord.MessageEmbed()
+        .addField('Error', 'User Not Found.')
+        .setColor(this.client.embedColor))
+    })
+  }
+}
