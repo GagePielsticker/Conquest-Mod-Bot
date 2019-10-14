ecea33c59483cb682fb016bb648387f3154ee254
Luke-6723 <lukemorris2015@hotmail.com>
2019-10-14 23:23:33 +0200

    rewrote ping command in accordance with new command handler

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,10 +1,16 @@
 const { Command } = require('./')
 
-module.exports = new Command('ping', ['pong'], 'Get the response time of the bot.', (client, msg, args) => {
-  msg.channel.send({
-    embed: {
-      description: `PONG! \`${client.ws.ping}ms\``,
-      color: client.embedColor
-    }
-  })
-}, 0)
+module.exports = class Ping extends Command {
+  constructor (client) {
+    super(client, ['ping', 'pong'], 'Get the response time of the bot.', 0)
+  }
+
+  run (msg, args) {
+    msg.channel.send({
+      embed: {
+        description: `PONG \`${this.client.ws.ping}\`ms`,
+        color: this.client.embedColor
+      }
+    })
+  }
+}
