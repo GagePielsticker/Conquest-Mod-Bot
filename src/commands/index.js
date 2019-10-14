216797e2567363114e3bb862e8cacbb61293f249
Luke-6723 <lukemorris2015@hotmail.com>
2019-10-14 23:23:17 +0200

    refactored command handler and class to be more object oreinted

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,44 +1,48 @@
 const fs = require('fs')
 
-exports.commands = {}
-exports.commands.aliases = {}
 /**
  * Command Constructor to initialize a command
- * @param {string} name name of the command
- * @param {array} aliases aliases for the command
+ * @param {string || array} name name of the command
  * @param {String} description command description
  * @param {function} func command execution
  * @param {number} permission a number between 0 - 3.
- * @example
- * new Command('example', 'example command', (client, msg, args) => {
- *  msg.channel.send("Hi there!")
- * }, 0)
  *
  * @constructor
  */
 class Command {
-  constructor (name, aliases, description, func, permission) {
+  constructor (client, name, description, permission = 0) {
     this.name = name
-    this.aliases = aliases
     this.description = description
-    this.func = func
     this.permission = permission
+    this.client = client
+  }
 
-    exports.commands[this.name] = this
-    aliases.map(commandAlias => {
-      exports.commands.aliases[commandAlias] = this.name
-    })
-    console.log(`[Conquest] ${this.name} constructed.`)
-    return this
+  run (msg, args) {
+    console.log(this.name)
   }
 }
+
+/**
+ * Export Command Constructor
+ */
 exports.Command = Command
 
-exports.init = () => new Promise((resolve, reject) => {
+/**
+ * @description Initialises the commands ready for use by the bot
+ */
+exports.init = (client) => new Promise((resolve, reject) => {
   try {
     const files = fs.readdirSync('./src/commands/', 'utf-8')
-    files.map(file => { if (file !== 'index.js') require(`./${file}`) })
-    resolve(exports.commands)
+    files.map(file => {
+      if (file !== 'index.js') {
+        var _Command = require(`./${file}`)
+        var Command = new _Command(client)
+        if (typeof (Command.name) === 'string') client.commands.set(Command.name, Command)
+        else if (typeof (Command.name) === 'object') Command.name.map(commandName => client.commands.set(commandName, Command))
+        else throw TypeError('Command Name must be an Object or String')
+      }
+    })
+    resolve(true)
   } catch (e) {
     reject(e)
   }
