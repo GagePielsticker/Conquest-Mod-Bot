be74add588488b039f0ef22a3da323ee89abf55a
Luke-6723 <lukemorris2015@hotmail.com>
2019-10-14 10:06:07 +0200

    removed a logging line

--- src/functions/convertTime.js
+++ src/functions/convertTime.js
@@ -3,12 +3,12 @@
  */
 module.exports = (timeInSeconds) => {
   timeInSeconds = Number(timeInSeconds)
-  const years = Math.floor(timeInSeconds / 31556952)
-  const months = Math.floor((timeInSeconds / 2592000) % 12)
-  const days = Math.floor((timeInSeconds / 86400) % 7)
-  const hrs = Math.floor((timeInSeconds / 3600) % 24)
-  const mins = Math.floor(timeInSeconds % 3600 / 60)
-  const secs = Math.floor(timeInSeconds % 60)
+  const years = Math.floor(timeInSeconds / 31556952000)
+  const months = Math.floor((timeInSeconds / 2592000000) % 12)
+  const days = Math.floor((timeInSeconds / 86400000) % 7)
+  const hrs = Math.floor((timeInSeconds / 3600000) % 24)
+  const mins = Math.floor(timeInSeconds % 3600000 / 60000)
+  const secs = Math.floor(timeInSeconds % 60000)
 
   var output = []
   if (years > 0) output.push(`${years.toString()} Year(s)`)
@@ -17,6 +17,5 @@ module.exports = (timeInSeconds) => {
   if (hrs > 0) output.push(`${hrs.toString()} Hour(s)`)
   if (mins > 0) output.push(`${mins.toString()} Min(s)`)
   if (secs > 0) output.push(`${secs.toString()} Second(s)`)
-  console.log(output)
   return output.join(', ')
 }
