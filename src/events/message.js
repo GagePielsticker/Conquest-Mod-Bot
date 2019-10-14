45e9cebbc7e96b0466dc278d37d699f3d2c3a2a9
Luke-6723 <lukemorris2015@hotmail.com>
2019-10-14 10:05:44 +0200

    instead of checking where you have said role, I check where you are in permission heirachy

--- src/events/message.js
+++ src/events/message.js
@@ -5,14 +5,17 @@ module.exports = (client, msg) => {
   if (msg.guild.id !== '597553336044224522') return
   const userPerms = client.permissionCheck(client, msg.member)
   const prefix = 'cm!'
-  const splitMessage = msg.content.replace(/[ ]+/).split()
+  const splitMessage = msg.content.replace(/[ ]+/, ' ').split(' ')
   const givenPrefix = splitMessage[0].substring(0, 3).toLowerCase()
   const givenCommand = splitMessage[0].substring(3).toLowerCase()
   const args = splitMessage.splice(1)
   if (givenPrefix === prefix) {
     if (commands[givenCommand]) {
       if (userPerms[3]) commands[givenCommand].func(client, msg, args)
-      else if (userPerms[commands[givenCommand].permission]) commands[givenCommand].func(client, msg, args)
+      else if (userPerms >= commands[givenCommand].permission) commands[givenCommand].func(client, msg, args)
+    } else if (commands.aliases[givenCommand]) {
+      if (userPerms[3]) commands[commands.aliases[givenCommand]].func(client, msg, args)
+      else if (userPerms >= commands[commands.aliases[givenCommand]].permission) commands[commands.aliases[givenCommand]].func(client, msg, args)
     }
   }
 }
