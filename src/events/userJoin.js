d06cfedb1f26d0a68826a821d55f503d9428aa6b
Luke-6723 <lukemorris2015@hotmail.com>
2019-10-14 02:17:51 +0100

    Converted to discords built in class

--- src/events/userJoin.js
+++ src/events/userJoin.js
@@ -1,25 +1,12 @@
-module.exports = (user) => {
-  const logChannel = user.guild.channels.get('632494293851832320')
-  const userAge = (Date.now() - user.createdAt) / 1000
-  logChannel.send({
-    embed: {
-      title: 'User Joined:',
-      description: `${user.tag}`,
-      author: {
-        name: 'Conquest Mod Bot',
-        url: 'https://conquestsim.io/',
-        icon_url: `https://cdn.discordapp.com/embed/avatars/${user.avatar}`
-      },
-      footer: {
-        text: 'Conquest Mod Bot | https://conquestsim.io/'
-      },
-      fields: [
-        {
-          name: 'Account Age',
-          value: `${user.client.convertTime(userAge)}`
-        }
-      ],
-      color: 0xb545747
-    }
-  })
+module.exports = (client, member) => {
+  const logChannel = member.guild.channels.get('632494293851832320')
+  const userAge = client.convertTime((Date.now() - member.user.createdAt) / 1000)
+
+  logChannel.send(
+    new client.discord.MessageEmbed()
+      .setDescription(`${member.user.tag} | <@${member.id}> | (${member.id})`)
+      .setAuthor('User Joined', member.user.avatarURL(), 'https://conquestsim.io/')
+      .addField('Account Age:', userAge)
+      .setColor(client.embedColor)
+  )
 }
