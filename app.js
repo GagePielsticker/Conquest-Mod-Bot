ce435a41e7e58b056cfc94b5b16d068e19373943
Luke-6723 <lukemorris2015@hotmail.com>
2019-10-14 05:05:04 +0100

    neaten up the commenting

--- app.js
+++ app.js
@@ -1,30 +1,21 @@
 // configure .env
 require('dotenv').config()
-// load src
+// Requirements
 const conquest = require('./src')
-// require discord
 const discord = require('discord.js')
-// define client
 const client = new discord.Client()
-// define conquest variable as a part of client
+
+// Store some useful variable in the client object
 client.conquest = conquest
-// define the embed color in client
 client.embedColor = 0xb54747
-// define convertTime function in client
 client.convertTime = conquest.convertTime
-// store discord module in client
 client.discord = discord
-// store eval user ids in an array on client object
 client.evalUsers = process.env.EVAL_USERS.split(' ')
-// store permissionCheck in client
 client.permissionCheck = conquest.permissionCheck
-// message event handler
+
+// Event Handlers
 client.on('message', (msg) => conquest.events.message(client, msg))
-
-// ready event handler
 client.on('ready', () => conquest.events.ready(client))
-
-// new guild member event handler
 client.on('guildMemberAdd', (member) => conquest.events.userJoin(client, member))
 
 // Login
