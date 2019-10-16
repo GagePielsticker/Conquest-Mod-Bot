e75f58ea1aa2bf0fefae2d6cab612cc3ffcac714
Luke-6723 <lukemorris2015@hotmail.com>
2019-10-16 09:28:26 +0200

    added some new functions

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ module.exports = {
   events: {
     ready: require('./events/ready'),
     userJoin: require('./events/userJoin'),
-    moderation: require('./events/moderation'),
-    message: require('./events/message')
+    message: require('./events/message'),
+    starboard: {
+      starAdded: require('./events/emoji').starAdded,
+      starRemoved: require('./events/emoji').starRemoved
+    },
+    moderation: {
+      userKicked: require('./events/moderation').userKicked,
+      userBanned: require('./events/moderation').userBanned
+    }
   },
   permissionCheck: require('./functions/permissionCheck'),
   convertTime: require('./functions/convertTime')
