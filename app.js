0cc5eb3e798163c85c7c2c88f15ce75aeff17305
Luke-6723 <lukemorris2015@hotmail.com>
2019-10-16 09:28:13 +0200

    Implement some more client functions and prep for starboard

--- app.js
+++ app.js
@@ -19,13 +19,42 @@ client.discord = discord
 client.evalUsers = process.env.EVAL_USERS.split(' ')
 client.permissionCheck = conquest.permissionCheck
 client.commands = new discord.Collection()
-client.conquestModerationHandlers = conquestModerationHandlers
+client.conquestModLogHandler = new conquestModerationHandlers.Modlog(client)
 client.conquestCouchDatabase = conquestCouchDatabase
+
+// client functions that will be used more than once [ Saving the copy paste drama ] // Feel free to clean this up with a PR
+client.getMultiplier = async (timeSpan) => {
+  switch (timeSpan) {
+    case ['month', 'months'].includes(timeSpan.toLowerCase()):
+      return 2629746
+    case ['d', 'days', 'day'].includes(timeSpan.toLowerCase()):
+      return 86400
+    case ['hr', 'hrs', 'hour', 'hours'].includes(timeSpan.toLowerCase()):
+      return 3600
+    case ['m', 'min', 'mins', 'minute', 'minutes'].includes(timeSpan.toLowerCase()):
+      return 60
+  }
+}
+
+client.checkTime = async (timeAbbreviation) => {
+  const times = timeAbbreviation.split(',')
+  let totalTime = 0
+  times.map(time => {
+    totalTime += (Number.parseInt(/[aA-zZ]+/g) * client.getMultiplier(time.replace(/[0-9]+/g, '')))
+  })
+  return totalTime
+}
 
 // Event Handlers
 client.on('message', (msg) => conquest.events.message(client, msg))
 client.on('ready', () => conquest.events.ready(client))
 client.on('guildMemberAdd', (member) => conquest.events.userJoin(client, member))
+client.on('guildMemberRemove', (member) => conquest.events.moderation.userKicked(client, member))
+client.on('guildBanAdd', (guild, user) => conquest.events.moderation.userBanned(client, user))
+
+// starboard events
+client.on('messageReactionAdd', () => conquest.events.ready(client))
+client.on('messageReactionRemove', () => conquest.events.ready(client))
 
 // Login
 client.login(process.env.BOT_TOKEN)
